97114f033e4f6a6051b6236218e7b20e6dbc9582
mkang <mkang@eecs.berkeley.edu>
2022-05-01 15:33:12 -0700

    [datset] fix issues with empty sequences

--- dataset/collect_results.py
+++ dataset/collect_results.py
@@ -8,15 +8,14 @@ def lines_that_contain(string, fp):
     return [line for line in fp if string in line]
 
 def merge_results_from_dir(dirname):
-    data = {}
+    data = []
     subdirs =  glob.glob(os.path.normpath(dirname + "/tab_*"))
     print("Found {} sub-directories".format(len(subdirs)))
     
     # Get data from Yosys and Vivado logs
-    i = 0
     for subdir in subdirs:
         this_data = {}
-        json_file = glob.glob(os.path.normpath(subdir + "/*.json"))    
+        json_file = glob.glob(os.path.normpath(subdir + "/*.json"))
         try:
             with open(json_file[0], "r") as fp:
                 this_data = json.load(fp)
@@ -24,11 +23,11 @@ def merge_results_from_dir(dirname):
             print("Could not open/read file:", subdir)
             continue
         seq = this_data["Sequence"]
+        if this_data["Sequence"] == "dch -f;if -K 6 -v;mfs" or this_data["Sequence"] == "":
+            continue
         if seq.find('dch') >= 0:
             this_data["Sequence"] = seq[:seq.find('dch')-1]
-#        this_data["Benchmark"] = this_data.pop("Becnhmark")
-        data[i] = this_data
-        i += 1
+        data.append(this_data)
     return data
 
 
@@ -40,7 +39,8 @@ def main():
     dir = os.path.abspath(args.input)
     summary_name = os.path.basename(dir)
     data = merge_results_from_dir(dir)
-    sorted_data = {k: v for k, v in sorted(data.items(), key=lambda i: (i[1]["Benchmark"], i[1]["Index"]))}
+    #sorted_data = {k: v for k, v in sorted(data.items(), key=lambda i: (i[1]["Benchmark"], i[1]["Index"]))}
+    sorted_data = sorted(data, key=lambda i: (i["Benchmark"], i["Index"]))
 
     with open(summary_name+".pkl", "wb") as handle:
         pickle.dump(sorted_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
